bbc227d427320f368a5cb483fcc7ec9b2d5a894d
Geoff Bourne <geoff.bourne@rackspace.com>
2017-08-08 13:41:02 -0500

    Close Ignite via application context between full unit tests

--- src/test/java/com/rackspace/telegrafhomebase/services/TaggingRepositoryTest.java
+++ src/test/java/com/rackspace/telegrafhomebase/services/TaggingRepositoryTest.java
@@ -12,6 +12,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.util.MultiValueMap;
 
@@ -39,6 +40,7 @@ import static org.junit.Assert.assertThat;
 }, properties = {
         "logging.level.org.apache.ignite.internal.processors.query.h2=debug"
 })
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
 public class TaggingRepositoryTest {
 
     @Autowired
